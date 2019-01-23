999532ce6bbc28c1cb204bab85fa654eb2a3d6c3
NilayBarde <nilaybarde@gmail.com>
2019-01-22 21:29:59 -0500

    Update BankCSVReader.java

--- BankCSVReader.java
+++ BankCSVReader.java
@@ -1,3 +1,12 @@
+/*In big data environments, analysts are constantly writing ad hoc database queries
+to obtain general table statistics for purposes of validation, trend analysis, and to simply gain a deeper
+understanding of their data. Suppose we had a .CSV text file listing bank customers. Each line includes
+the customer’s account number, name, marital status and account balance. The bank manager would
+like to know if customers with different marital status have, on average, significantly different account
+balances. In a programming language of your choice (or pseudocode), write or describe step-by-step a
+function that processes the .CSV file and outputs the average account balance for customers in each
+marital status category. */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
